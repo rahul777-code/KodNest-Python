988a20ff0a68e29ded36c6030ad69a63d8ce71db
surakani rahul <surakanirahul@gmail.com>
2026-08-06 16:15:21 +0530

    display number sequence and word character

---
+++ track/M02-Introduction-to-python/M02-T02-conditinal_statements_and_loops/ST11-display_number_sequnce_and_word_character.py
@@ -0,0 +1,10 @@
+N = int(input("Enter the Number:"))
+word = input("Enter the word:")
+
+print("Numbers:")
+for i in range(1, N + 1):
+    print(i)
+
+print("Characters:")
+for char in word:
+    print(char)
