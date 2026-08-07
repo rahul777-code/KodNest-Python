3701b2c3d0f9494fb412c4befedb004a480535d5
surakani rahul <surakanirahul@gmail.com>
2026-08-07 16:06:31 +0530

    update a tuple without modifying it

---
+++ track/M02-Introduction-to-python/M02-T03-list_tuples_strings_slicing_mutability/ST10-update_a_tuple_without_modifying_it.py
@@ -0,0 +1,12 @@
+course_name = input()
+course_week = input()
+course_status = input()
+
+course_details = (course_name, course_week,course_status)
+
+updated_week = input()
+
+course_details = course_details[0], updated_week, course_details[2]
+
+print(course_details)
+
